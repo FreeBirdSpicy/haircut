9bd9cde7d268c019b979f20946a2c36c583bc900
Enzo <271630105@qq.com>
2025-09-27 00:49:07 +0800

    项目列表改用rpc获取数据

--- controllers/admin/ticketController.go
+++ controllers/admin/ticketController.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 	"hs_project/models"
 	"net/http"
+	"net/rpc/jsonrpc"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type TicketController struct{}
+
+type TiecktsReq struct {
+	Page    int    `json:"page" default:"1"`
+	Limit   int    `json:"limit" default:"10"`
+	State   int    `json:"state" default:"1"`
+	Keyword string `json:"keyword" default:""`
+}
+
+type TicketsRes struct {
+	Total   int64           `json:"total"`
+	Data    []models.Ticket `json:"data"`
+	Page    int             `json:"page"`
+	Keyword string          `json:"keyword"`
+}
 
 // 列表
 func (ac TicketController) Index(c *gin.Context) {
@@ -32,27 +47,57 @@ func (ac TicketController) Index(c *gin.Context) {
 	if err != nil || limit < 1 {
 		limit = 10
 	}
-	start := (page - 1) * limit
+	// start := (page - 1) * limit
 
-	tickets := []models.Ticket{}
-	var total int64
+	// tickets := []models.Ticket{}
+	// var total int64
 
-	// 获取数据总数和分页数据
-	if keyword != "" {
-		models.DB.Model(&models.Ticket{}).Where("state = ? AND name LIKE ?", state, "%"+keyword+"%").Count(&total)
-		models.DB.Where("state = ? AND name LIKE ?", state, "%"+keyword+"%").Limit(limit).Offset(start).Find(&tickets)
-	} else {
-		models.DB.Model(&models.Ticket{}).Where("state = ?", state).Count(&total)
-		models.DB.Where("state = ?", state).Limit(limit).Offset(start).Find(&tickets)
-	}
+	// // 获取数据总数和分页数据
+	// if keyword != "" {
+	// 	models.DB.Model(&models.Ticket{}).Where("state = ? AND name LIKE ?", state, "%"+keyword+"%").Count(&total)
+	// 	models.DB.Where("state = ? AND name LIKE ?", state, "%"+keyword+"%").Limit(limit).Offset(start).Find(&tickets)
+	// } else {
+	// 	models.DB.Model(&models.Ticket{}).Where("state = ?", state).Count(&total)
+	// 	models.DB.Where("state = ?", state).Limit(limit).Offset(start).Find(&tickets)
+	// }
+
+	// c.HTML(http.StatusOK, "admin/ticket.html", gin.H{
+	// 	"title":   "项目列表",
+	// 	"data":    tickets,
+	// 	"page":    page,
+	// 	"total":   total,
+	// 	"keyword": keyword,
+	// })
+
+	req := TiecktsReq{Page: page, Limit: limit, State: state, Keyword: keyword}
+	var res TicketsRes
+	getTicketsList(req, &res)
 
 	c.HTML(http.StatusOK, "admin/ticket.html", gin.H{
 		"title":   "项目列表",
-		"data":    tickets,
-		"page":    page,
-		"total":   total,
-		"keyword": keyword,
+		"data":    res.Data,
+		"page":    res.Page,
+		"total":   res.Total,
+		"keyword": res.Keyword,
 	})
+}
+
+func getTicketsList(req TiecktsReq, res *TicketsRes) {
+	// 1、连接远程rpc服务
+	conn, err1 := jsonrpc.Dial("tcp", "127.0.0.1:8081")
+	if err1 != nil {
+		fmt.Println("Dial err:", err1)
+		return
+	}
+
+	// 2、客户端退出时关闭连接
+	defer conn.Close()
+
+	// 3、调用远程函数
+	err2 := conn.Call("Ticket.GetTicketsInfo", req, &res)
+	if err2 != nil {
+		fmt.Println("Call err:", err2)
+	}
 }
 
 func (ac TicketController) Edit(c *gin.Context) {
